e4c995d75ace171dcb6c542a7bc32d311606f016
saltyBanana <bensaltmiras@gmail.com>
2016-02-17 16:30:53 -0600

    Stuff changed feb. 17 2016
    Added autonomous modes class, launcher class, Haden's gyro class,
    started teleop functions

--- src/org/usfirst/frc/team4256/robot/Robot.java
+++ src/org/usfirst/frc/team4256/robot/Robot.java
@@ -1,17 +1,16 @@
 
 package org.usfirst.frc.team4256.robot;
 
+
 import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.CANTalon;
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+//import edu.wpi.first.wpilibj.Compressor;
+//import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Servo;
-import edu.wpi.first.wpilibj.VictorSP;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -25,6 +24,9 @@ public class Robot extends IterativeRobot {
 	static DBJoystick xboxDriver = new DBJoystick(0);
 	static DBJoystick xboxGun = new DBJoystick(1);
 	
+	//Toggles
+//	static Toggle scissorToggle = new Toggle(xboxGun, 4);
+	
 	//Drive
 	static Drive4256 drive;
 //	static CANTalon wheelFrontLeft;
@@ -33,11 +35,9 @@ public class Robot extends IterativeRobot {
 //	static CANTalon wheelBackRight;
 //	static RobotDrive drive = new RobotDrive(1, 2, 3, 4);
 	//Launch
-	static CANTalon turretRotator = new CANTalon(7);
-	static CANTalon shootingWheel = new CANTalon(8);
+//	static CANTalon turretRotator = new CANTalon(7);
+//	static CANTalon shootingWheel = new CANTalon(8);
 	//Intake
-	static Lifter intakeLifter;
-	static Intake intake;
 //	static CANTalon intakeLeft = new CANTalon(5);
 //	static CANTalon intakeRight = new CANTalon(6);
 //	static VictorSP stagingRollerLeft = new VictorSP(0);
@@ -45,37 +45,52 @@ public class Robot extends IterativeRobot {
 //	static VictorSP intakeRoller = new VictorSP(1);
 	
 	//Servos
-	static Servo servoX = new Servo(0);
-	static Servo servoY = new Servo(1);
-	static Camera cameraServos = new Camera(servoX, servoY, (int)6.9);
+//	static Servo servoX = new Servo(0);
+//	static Servo servoY = new Servo(1);
+//	static Camera cameraServos = new Camera(servoX, servoY, (int)6.9);
 	
 	//Solenoids
-	static Compressor compressor = new Compressor();
-	static AnalogInput PressureGauge = new AnalogInput(0);
-	static DoubleSolenoid gearShift = new DoubleSolenoid(0, 2, 4);
-	static DoubleSolenoid scissorLift = new DoubleSolenoid(1, 2, 4);
+//	static Compressor compressor = new Compressor();
+//	static AnalogInput PressureGauge = new AnalogInput(0);
+//	static DoubleSolenoid gearShift = new DoubleSolenoid(0, 2, 4);
+//	static DoubleSolenoid scissorLift = new DoubleSolenoid(1, 2, 4);
 	
 	//Relays
 	static Relay light = new Relay(0);
 	
 	//DI
-	static DigitalInput stagingAreaSensor = new DigitalInput(0);
-	static DigitalInput leftIntakeBumper = new DigitalInput(1);
-	static DigitalInput rightIntakeBumper = new DigitalInput(2);
-	
+//	static DigitalInput stagingAreaSensor = new DigitalInput(0);
+//	static DigitalInput leftIntakeBumper = new DigitalInput(1);
+//	static DigitalInput rightIntakeBumper = new DigitalInput(2);
+	//AI
+	static Gyro4256 gyro = new Gyro4256(new AnalogInput(0));
 
+
+	static NetworkTable visionTable;
 	
+	static Lifter intakeLifter;
+	static Intake intake;
+	static Launcher launcher;
+	
+	static Gamemode gamemode;
+	static enum Gamemode {AUTONOMOUS, TELEOP};
 	
 	//static Launcher robotLauncher;
 	
 	public void robotInit() {
-		drive = new Drive4256(new RobotDrive(1, 2, 3, 4), new DoubleSolenoid(0, 2, 4), new DoubleSolenoid(1, 3, 5));
-		intakeLifter = new Lifter(7, 6, 000/*unknown*/, 000/*unknown*/);
-		intake = new Intake(4, 5, 8, 000/*unknown*/);
+		visionTable = NetworkTable.getTable("SaltVision");
+//		
+		drive = new Drive4256(new RobotDrive(0, 1, 2, 3), new DoubleSolenoid(0, 2, 4), new DoubleSolenoid(1, 2, 4));
+//		drive = new Drive4256(new RobotDrive(1, 2, 3, 4), null, null);
+		intakeLifter = new Lifter(7, 6, 000/*unknown*/, 001/*unknown*/);
+//		
+		intake = new Intake(4, 5, 8, 003/*unknown*/);
+		launcher = new Launcher(7, 8, new DoubleSolenoid(5, 2, 4), visionTable);
+		
 	}
 
 	public void autonomousInit() {
-
+		gamemode = Gamemode.AUTONOMOUS;
 	}
 
 	/**
@@ -90,9 +105,45 @@ public class Robot extends IterativeRobot {
 	 */
 	long timeSinceLaunchStart;
 	public void teleopPeriodic() {
-		drive.arcadeDrive(xboxDriver.getRawAxis(1), xboxDriver.getRawAxis(4));
-	}
-	
+		gamemode = Gamemode.TELEOP;
+		
+		//Drive
+		drive.arcadeDrive(xboxDriver.getRawAxis(DBJoystick.AXIS_LEFT_Y), xboxDriver.getRawAxis(DBJoystick.AXIS_RIGHT_X));
+		if(xboxDriver.getRawButton(DBJoystick.BUTTON_LB)) {
+			drive.gearShift();
+		}
+		
+		//Intake Lifter
+		
+		//Intake
+		if(xboxGun.getRawButton(DBJoystick.BUTTON_RB)) {
+			//stager up
+		}
+		if(xboxGun.getRawButton(DBJoystick.BUTTON_A)) {
+			intake.intakeIn();
+		}else if(xboxGun.getRawButton(DBJoystick.BUTTON_X)) {
+			intake.intakeOut();
+		}
+		
+		//Launcher
+		if(xboxGun.getRawButton(DBJoystick.AXIS_RT)) {
+			launcher.clockwiseTurret();
+		}else if(xboxGun.getRawButton(DBJoystick.AXIS_LT)) {
+			launcher.counterClockwiseTurret();
+		}else{
+			launcher.turretStop();
+		}
+		//Gyro
+		//gyro.rotateToAngle(SmartDashboard.getNumber("goalAngle"))
+		
+		//ScissorLift
+//		if(scissorToggle.getState()) {
+//			scissorLift.set(DoubleSolenoid.Value.kForward);
+//		}else{
+//			scissorLift.set(DoubleSolenoid.Value.kReverse);
+//		}
+		
+		}
 	/**
 	 * This function is called periodically during test mode
 	 */
